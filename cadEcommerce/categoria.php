19f783ee315c9e08b92a28fd7ae7a47f0d8a547a
Lara <lara.assuncao@alunoescolassociais.org.br>
2024-08-05 09:25:49 -0300

    Adiciona arquivos já prontos ao arquivo README.md

---
+++ cadEcommerce/categoria.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cadastrar categoria</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <header>  
+    <div class="center">
+<h1>cadastro categoria<h1>
+    <a href="index.php" target="_self">voltar</a>
+</div>
+</header>
+<section id="produtos">
+    <form action="insere-categoria.php" method="post">
+    <label for="">descrição:</label>
+        <input type="text" name="descricao">
+        <input type="submit" name="cadastrar">
+       
+       
+       
+
+
+</form>     
+
+</section>
+</body>
+</html>
---
+++ cadEcommerce/insere-marca.php
@@ -0,0 +1,11 @@
+<?php
+include('controller/conexao.php');
+$descricao = $_POST['descricao'];
+echo "<h3>Descricão: $descricao</h3>";
+$cad_categoria = "INSERT INTO marca(DESCRICAO) VALUES ('$descricao')";
+if(mysqli_query($mysqli, $cad_categoria)){
+    echo "<h1>Categoria cadastrada com sucesso!<h1><br>";
+}else{
+    echo "erro: " . "<br>" . mysqli_error($mysqli);
+}mysqli_close($mysqli);
+?>
